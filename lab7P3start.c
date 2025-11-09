db5481f5477a1a50fecfae3e739a89b2b0f8c85b
John.McLean <johnmclean040@gmail.com>
2025-11-09 15:44:53 -0500

    fix

--- lab7P3start.c
+++ lab7P3start.c
@@ -2,29 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    char input[256];
-    int letterCount[26] = {0}; // Array to store the count of each letter initialized to zeros
+int main() {
+  char input[256];
+  int letterCount[26] = {
+      0};  // Array to store the count of each letter initialized to zeros
 
-    printf("Enter text strings (press Enter on an empty line to exit):\n");
+  printf("Enter text strings (press Enter on an empty line to exit):\n");
 
-    while (1)
-    {
-        fgets(input, sizeof(input), stdin); // get the input from the user via keyboard
+  while (1) {
+    fgets(input, sizeof(input),
+          stdin);  // get the input from the user via keyboard
 
-        // Check for an empty line to exit the loop
-        if (strlen(input) == 1 && input[0] == '\n')
-        {
-            break;
+    // Check for an empty line to exit the loop
+    if (strlen(input) == 1 && input[0] == '\n') {
+      break;
+    } else {
+      for (int j = 0; j < strlen(input); j++) {
+        char count = input[j];
+        if (count >= 'A' && count <= 'Z') {
+          letterCount[count - 'A']++;
+        } else if (count >= 'a' && count <= 'z') {
+          letterCount[count - 'a']++;
         }
-        // Your Code should be implemented here
-        // Iterate through the characters in the input
-        // Check if the character is a letter (A-Z or a-z)
-        // Increment the corresponding count in the array
+      }
+      printf("Letter distrubtion of %s\n", input);
+      for (int i = 0; i < 26; i++) {
+        printf("%c: %d ", 'A' + i, letterCount[i]);
+      }
     }
-    // More of your code here to
-    // Display the letter counts
+    // Your Code should be implemented here
+    // Iterate through the characters in the input
+    // Check if the character is a letter (A-Z or a-z)
+    // Increment the corresponding count in the array
+  }
+  // More of your code here to
+  // Display the letter counts
 
-    return 0;
+  return 0;
 }
